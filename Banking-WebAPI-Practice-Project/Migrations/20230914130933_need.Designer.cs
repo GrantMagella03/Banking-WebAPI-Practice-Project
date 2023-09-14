d0880afd0cfbdb94bdca68c420c6c016da74848d
Grant Magella <Bitman2003@gmail.com>
2023-09-14 09:10:30 -0400

    migrations

--- Banking-WebAPI-Practice-Project/Migrations/20230914130933_need.Designer.cs
+++ Banking-WebAPI-Practice-Project/Migrations/20230914130933_need.Designer.cs
@@ -4,6 +4,7 @@ using Banking_WebAPI_Practice_Project.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Banking_WebAPI_Practice_Project.Migrations
 {
     [DbContext(typeof(Banking_WebAPI_Practice_ProjectContext))]
-    partial class Banking_WebAPI_Practice_ProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20230914130933_need")]
+    partial class need
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Banking-WebAPI-Practice-Project/Migrations/20230914130933_need.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Banking_WebAPI_Practice_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class need : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountID = table.Column<int>(type: "int", nullable: false),
+                    PreviousBalance = table.Column<decimal>(type: "decimal(11,2)", nullable: false),
+                    TransactionType = table.Column<string>(type: "nvarchar(1)", maxLength: 1, nullable: false),
+                    NewBalance = table.Column<decimal>(type: "decimal(11,2)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: true),
+                    CreationDate = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Accounts_AccountID",
+                        column: x => x.AccountID,
+                        principalTable: "Accounts",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_AccountID",
+                table: "Transactions",
+                column: "AccountID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
